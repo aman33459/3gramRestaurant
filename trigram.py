057d4dfe37a5e76a496a6a5416f294dc8c306af7
Aman Mahansaria <aman33459@gmail.com>
2020-04-06 19:47:58 +0530

    changes

--- trigram.py
+++ trigram.py
@@ -22,16 +22,44 @@ def count(st,text):
 				cn=cn+1
 	return cn
 def totalvocab(text):
+    text=open('transcript.txt','r')
     words=text.read()
-    return len(set(words.split()))
+    k= len(set(words.split()))
+    #print(k)
+    return k
 
+def count1(s , text):
+    text=open('transcript.txt','r')
+    words = text.read()
+    cn=0
+    #print(words)
+    #print(len(words.split()))
+    for word in words:
+       # print(word)
+        if(s.lower() == word.lower()):
+            cn+=1
+   # print('***')
+    #print(cn / len(words.split()))
+    return (cn+1)/(len(words.split())+totalvocab(text))
+
+def count2(s1,s2):
+    text=open('transcript.txt','r')
+    words=text.read()
+    cn = 0
+    for word in words:
+        if(word.lower() == s1.lower()):
+            cn+=1
+    text=open('transcript.txt','r')
+    words=text.read()
+    cn1=0
+    for word in range(0,len(words)-1,2):
+        if(words[word].lower() ==s1.lower() and  s2.lower()==words[word+1].lower()):
+            cn1+=1
+    return (cn1+1)/(cn+totalvocab(s1))
 
 print('Prediction of occurence of word in a given trigram')
 val = input('Enter the sentence\n')
 words = val.split(' ')
-if(len(words) <=2):
-	print('Trigram model would not predict for sentences having length less than three')
-	sys.exit(0)
 cal = dict()
 word_space=[]
 k=-1
@@ -48,6 +76,8 @@ res=1.000
 v= totalvocab(text)
 st.append(word_space[0].lower())
 st.append(word_space[1].lower())
+res = res * count1(word_space[0] , text)
+res = res * count2(word_space[0], word_space[1])
 for i in range(2,len(word_space)):
 	text=open('transcript.txt','r')
 	tmp=text
